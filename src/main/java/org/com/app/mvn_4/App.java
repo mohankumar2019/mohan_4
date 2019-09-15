262375c453fe9774ad0a63a41afe8a3a9a2fd78c
mohan <mohan.chanti4@gmail.com>
2019-09-15 09:41:41 +0530

    new changes

---
+++ src/main/java/org/com/app/mvn_4/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvn_4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvn_4" );
+    }
+}
